49c2780f8a3864f54a4acb7af1fc4421a4db7dc2
Scott Burbidge <scottisreally@gmail.com>
2014-08-03 13:06:08 -0700

    Ajax final maybe

--- students/scottBurbidge/tabs Ajax/tabsAjax.js
+++ students/scottBurbidge/tabs Ajax/tabsAjax.js
@@ -1,53 +1,16 @@
-var tab
-var content
-var data
-
 $.getJSON('http://rs.hankyates.com:3000/content', function(data){
-  clip = data.name;
-  content = data.content;
   console.log(data);
-  $.each(data, function(clip, value){
-       $(".tab").append(clip);
+  var ii = 1
+  data.forEach(function(data){
+    $(".nav").append('<li class="tab" value=' + ii++ + '>' + data['name'] + '</li>');
+    $(".content").append("" + data['content'] + "");
   });
-});  
-
-function clickHandler() {
-  $(this).next().addClass("image").removeClass("noImage");
-  $(this).removeClass("image").addClass("noImage");
-};  
-
-function lastImage(){
-  $(".first").addClass("image").removeClass("noImage");
-  $(this).removeClass("image").addClass("noImage"); 
-};
-
-function ageList(){
-  var lowEnd = 0;
-  var highEnd = 99;
-  var ageArr = [];
-  while(lowEnd <= highEnd){
-     ageArr.push(lowEnd++);
-  };
-return ageArr
-};
-
+});
 
 $(document).ready(function(){
-  var age  = ageList();
-  var aList = $('select');
-  $.each(age, function(i){
-  $('<option></option>')
-      .val([i + 1])
-      .text([i + 1])
-      .appendTo(aList);
-  });
-  
   $(".tab").click(function(){
     var wind = $(this).val();
     $(".pane").addClass("paneGone").removeClass("paneSelect");
     $("#in" + wind).removeClass("paneGone").addClass("paneSelect");
   })
-    
-  $(".clickable").click(clickHandler);
-  $(".last").click(lastImage);
 })
